b2106cbdc9dfd8787cec7452533120e853ae257f
Bill McCormick <wmccormick@swri.org>
2019-03-18 22:29:17 -0500

    WIP add devicedata to local include

---
+++ eip_device/include/eip_device/devicedata.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2018, Rockwell Automation, Inc.
+ * All rights reserved.
+ *
+ ******************************************************************************/
+ 
+#ifndef DEVICE_DATA_H_
+#define DEVICE_DATA_H_
+ 
+#define OPENER_DEVICE_VENDOR_ID           1
+#define OPENER_DEVICE_TYPE               12
+#define OPENER_DEVICE_PRODUCT_CODE      65001
+#define OPENER_DEVICE_MAJOR_REVISION      0
+#define OPENER_DEVICE_MINOR_REVISION      1
+#define OPENER_DEVICE_NAME ("ROS")
+
+#endif /* DEVICE_DATA_H_ */
